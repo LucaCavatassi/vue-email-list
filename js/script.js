deaf12b4d66918db70668c4dc4386e4cceccc5c7
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-29 15:55:42 +0200

    fix: not using arrow function on the method, let this not working, cause remained into his scope

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            email: "",
+        };
+    },
+    created(){
+        this.rndmEmail();
+    },
+    methods: {
+        rndmEmail: function() {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    console.log(resp);
+                    this.email = resp.data.response;
+                })
+            }
         }
-    }
 }).mount('#app')
